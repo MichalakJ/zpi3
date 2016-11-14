65001f4b1304bdc4269bd4e65fee8ed0c421b07c
Jakub Michalak <daran.dasaner@gmail.com>
2016-11-14 18:41:34 +0100

    added test for incorrect destination path

--- mod1/src/test/java/FileDownloader/FileDownloaderTest.java
+++ mod1/src/test/java/FileDownloader/FileDownloaderTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.InvalidPathException;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
@@ -19,10 +20,16 @@ public class FileDownloaderTest {
         FileDownloader.downloadFile("http://www.textfiles.com/games/abc.txt", "test.txt");
         assertThat(new File("test.txt").exists(), equalTo(true));
     }
-    
+
     @Test(expected = MalformedURLException.class)
     public void incorrectURL_downloadFile_throwsMalformedURLException() throws IOException {
         FileDownloader.downloadFile("gaws", "E:\\tests\\test2.txt");
+    }
+
+
+    @Test(expected = InvalidPathException.class)
+    public void incorrectDestination_downloadFile_throwsMalformedURLException() throws IOException {
+        FileDownloader.downloadFile("http://www.textfiles.com/games/abc.txt", "X:\\X:\\X:\\");
     }
 
 }
