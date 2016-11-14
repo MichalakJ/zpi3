34ad1f0e818103d6aa2ab0ddcdb7b9067cc4f0a5
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-11-14 22:45:12 +0100

    md5 checksum method

---
+++ mod2/src/main/java/ChecksumUtils.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+
+/**
+ * @Author Mateusz Wieczorek on 11/14/16.
+ */
+public class ChecksumUtils {
+
+    public static String calculateMD5Checksum(File file) throws NoSuchAlgorithmException {
+        FileInputStream fis = null;
+        String md5 = "";
+        try {
+            fis = new FileInputStream(file);
+            md5 = md5Hex(fis);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return md5;
+    }
+
+
+}
