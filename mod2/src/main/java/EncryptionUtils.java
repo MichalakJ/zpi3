9568d74054512de44e113c96967841a727b8aaee
Mateusz Wieczorek <wskmateusz@gmail.com>
2016-11-14 22:06:01 +0100

    read file method

---
+++ mod2/src/main/java/EncryptionUtils.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @Author Mateusz Wieczorek on 11/14/16.
+ */
+public class EncryptionUtils {
+
+    private static List<String> readFile(File file) {
+        try (Stream<String> stream = Files.lines(Paths.get(file.getAbsolutePath()))) {
+            return stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
