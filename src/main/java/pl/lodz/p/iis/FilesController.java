136e5ccc111dd7c643ee52547c26d46ee8a87835
Jakub Michalak <daran.dasaner@gmail.com>
2016-12-05 17:43:54 +0100

    implemented http method for unziping archive

--- src/main/java/pl/lodz/p/iis/FilesController.java
+++ src/main/java/pl/lodz/p/iis/FilesController.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.iis;
 
 import FileDownloader.FileDownloader;
+import ZipManager.ZipManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,5 +30,29 @@ public class FilesController {
             return new ResponseEntity<PostFile>(file, HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<PostFile>(file, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/files/{name}/actions.zip", method = RequestMethod.POST)
+    public ResponseEntity<String> zipFile(@PathVariable String name, @RequestBody String zipName){
+        return new ResponseEntity<String>("not implemented", HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @RequestMapping(value = "/files/{name}/actions.unzip", method = RequestMethod.POST)
+    public ResponseEntity<String> unzipFile(@PathVariable String name){
+        String zipFileDir = ROOT_LOCATION + File.separator + name;
+        File file = new File(zipFileDir);
+
+        if(!file.exists()) {
+            return new ResponseEntity<String>("file not found", HttpStatus.NOT_FOUND);
+        }
+        if(!name.endsWith(".zip")){
+            return new ResponseEntity<String>("not a zip file", HttpStatus.BAD_REQUEST);
+        }
+        try {
+            ZipManager.unzip(zipFileDir, ROOT_LOCATION);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("could not unzip file: "+ e.getMessage(), HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<String>("", HttpStatus.OK);
     }
 }
