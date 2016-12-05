00bf6f732a660f0d9d62a6b3e028106a40296f29
Jakub Michalak <daran.dasaner@gmail.com>
2016-12-05 17:25:46 +0100

    Implemented method for posting new files

--- src/main/java/pl/lodz/p/iis/FilesController.java
+++ src/main/java/pl/lodz/p/iis/FilesController.java
@@ -1,10 +1,11 @@
 package pl.lodz.p.iis;
 
 import FileDownloader.FileDownloader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.lodz.p.iis.library.FileList;
+import pl.lodz.p.iis.library.PostFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,5 +19,15 @@ public class FilesController {
         FileList fileList = new FileList();
         fileList.setFileList(root.list());
         return fileList;
+    }
+
+    @RequestMapping(value = "/files", method = RequestMethod.POST)
+    public ResponseEntity<PostFile> addFile(@RequestBody PostFile file){
+        try {
+            FileDownloader.downloadFile(file.getUrl(), ROOT_LOCATION + File.separator + file.getName());
+        } catch (IOException e) {
+            return new ResponseEntity<PostFile>(file, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<PostFile>(file, HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/pl/lodz/p/iis/library/PostFile.java
@@ -0,0 +1,25 @@
+package pl.lodz.p.iis.library;
+
+/**
+ * Created by Jakub on 2016-12-05.
+ */
+public class PostFile {
+    String url;
+    String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
