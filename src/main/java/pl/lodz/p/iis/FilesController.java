262cd4e66bc0609e02247a35e081bfa19a749b1f
Jakub Michalak <daran.dasaner@gmail.com>
2016-12-05 17:08:43 +0100

    Implemented endpoint for listing files

--- src/main/java/pl/lodz/p/iis/FilesController.java
+++ src/main/java/pl/lodz/p/iis/FilesController.java
@@ -1,14 +1,22 @@
 package pl.lodz.p.iis;
 
+import FileDownloader.FileDownloader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.lodz.p.iis.library.FileList;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class FilesController {
-
+    private static final String ROOT_LOCATION = "C:\\files";
     @RequestMapping("/files")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello " + name;
+    public FileList greeting(@RequestParam(value="name", defaultValue="World") String name) throws IOException {
+        File root = new File(ROOT_LOCATION);
+        FileList fileList = new FileList();
+        fileList.setFileList(root.list());
+        return fileList;
     }
 }
---
+++ src/main/java/pl/lodz/p/iis/library/FileList.java
@@ -0,0 +1,21 @@
+package pl.lodz.p.iis.library;
+
+import FileDownloader.FileDownloader;
+
+import java.util.List;
+
+/**
+ * Created by Jakub on 2016-12-05.
+ */
+public class FileList {
+    String[] fileList;
+
+    public String[] getFileList() {
+        return fileList;
+    }
+
+    public void setFileList(String[] fileList) {
+        this.fileList = fileList;
+
+    }
+}
