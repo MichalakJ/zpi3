060df797461e4a3ba2459f901a9b54ad9925bbda
Jakub Michalak <daran.dasaner@gmail.com>
2016-12-06 06:25:45 +0100

    added tests for zipping file

--- mod1/src/test/java/ZipManager/ZipManagerTest.java
+++ mod1/src/test/java/ZipManager/ZipManagerTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Jakub on 2016-11-14.
@@ -23,5 +24,12 @@ public class ZipManagerTest {
         File file = new File(classLoader.getResource("zipTest.zip").getFile());
         ZipManager.unzip(file.getAbsolutePath(), file.getParent());
         assertThat(file.getParentFile().exists(), equalTo(true));
+    }
+
+    @Test
+    public void dirExist_zip_archiveCreated() {
+        String zip = "C:\\files\\zipeedIt.zip";
+        ZipManager.zip("C:\\files", zip);
+        assertThat(new File(zip).exists(), equalTo(true));
     }
 }
